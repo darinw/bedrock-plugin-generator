0950afc64618c3591c1805128041474c21da09fb
Darin <darin@darinwolyniec.com>
2020-04-28 11:20:45 -0500

    added some file reading stuff

--- index.py
+++ index.py
@@ -1,11 +1,35 @@
-from tkinter import *
+import tkinter as tk
+import os
 
-window = Tk()
+window = tk.Tk()
 window.title("Bedrock Plugin Generator")
-window.geometry('350x200')
+window.geometry('500x300')
 
-lbl = Label(window, text="Welcome to the generator")
+# DO THIS CELLO!
+# https://www.delftstack.com/howto/python-tkinter/how-to-set-window-icon-in-tkinter/
 
-lbl.grid(column=0, row=0)
+
+var1 = tk.StringVar()
+l = tk.Label(window, bg='green', fg='yellow',font=('Arial', 12), width=40, textvariable=var1)
+l.pack()
+
+def print_selection():
+    value = listbox.get(listbox.curselection())   
+    var1.set(value)  
+
+
+listbox = tk.Listbox(window, width=40)
+listbox.pack()
+listbox.insert(tk.END, "a list entry")
+
+#  this will allow you to read the files within the behavoir pack.
+#  it will be things like 'bedrock.json' or 'grass.json'
+#  from there we will read the file, make a modification and save it into a '_generated_packs' folder 
+for idx, fileName in enumerate(os.listdir("templateBehaviorPack")):
+    listbox.insert(tk.END, fileName)
+
+
+b1 = tk.Button(window, text='print selection', width=15, height=2, command=print_selection)
+b1.pack()
 
 window.mainloop()
