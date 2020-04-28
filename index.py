b2a41e68b5fc1f758c6191a3ea2471f141f2656d
Darin <darin@darinwolyniec.com>
2020-04-28 10:50:13 -0500

    created simple popup with tinker kit

---
+++ index.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.title("Bedrock Plugin Generator")
+window.geometry('350x200')
+
+lbl = Label(window, text="Welcome to the generator")
+
+lbl.grid(column=0, row=0)
+
+window.mainloop()
